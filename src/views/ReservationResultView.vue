edd5b94fefb2a51d77f2486fbe49925e0ecf42bd
nitorionedan <fearinatunitori@gmail.com>
2023-09-23 22:04:10 +0900

    dev: base of views

---
+++ src/views/ReservationResultView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Reservation Result</h1>
+  <RouterLink to="/sheetselecting">Back</RouterLink>
+</template>
---
+++ src/views/SheetSelectingView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Sheet selecting view</h1>
+  <RouterLink to="/reservationresult">Reserve</RouterLink>
+</template>
